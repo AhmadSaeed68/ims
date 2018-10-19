833bc890ea6dc7aa33be5c5d21c85e751ec4cb94
MusawerAli <pakjalihouse@gmail.com>
2018-10-19 22:00:18 +0500

    stock_by join Method

--- application/models/stock_model.php
+++ application/models/stock_model.php
@@ -1,10 +1,13 @@
 <?php
     class Stock_model extends CI_Model{
         function stock(){
-            $query=$this->db
-            ->select()
-            ->get('items_in_stock');
-return $query->result();
+//             $query=$this->db
+//             ->select()
+//             ->get('items_in_stock');
+// return $query->result();
+$data=$this->db
+->query('SELECT items.item_name,items.item_code,items.category_id,items.item_description,items_in_stock.po_code,items_in_stock.id,items_in_stock.invoice_code,items_in_stock.item_qty,items_in_stock.entry_date,items_in_stock.item_rate FROM items_in_stock LEFT JOIN items ON items.item_code=items_in_stock.item_code');
+return $data->result();
         }
     }
 ?>
