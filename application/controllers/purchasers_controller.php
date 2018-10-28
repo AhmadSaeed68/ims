cda836dc44ca99dccad37cfb749bd5172d056804
MusawerAli <pakjalihouse@gmail.com>
2018-10-28 21:06:24 +0500

    purchasers_controller

---
+++ application/controllers/purchasers_controller.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	class Purchasers_controller extends CI_Controller{
+
+		function purchasers_detail(){
+
+			$this->load->model('purchasers_model');
+			$data=$this->purchasers_model->purchasers_detail();
+		
+			$this->load->view('purchasers_view',['data'=>$data]);
+		}
+
+
+
+							//GET BUSINESS DATA
+					function business_data(){
+						 $business_name=$this->input->post('business_name');
+						 $this->load->model('purchasers_model');
+						 $data=$this->purchasers_model->business_data($business_name);
+						 $this->load->view('purchasers/business_data',['data'=>$data]);
+					}
+	}
+ ?>
