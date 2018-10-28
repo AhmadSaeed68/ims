846a7e063fdf321ba12b20d88e209d6bb9fcbd4c
MusawerAli <pakjalihouse@gmail.com>
2018-10-28 21:14:39 +0500

    fix some bugs in  foreach loop view_so

--- application/views/ajax_so/view_so.php
+++ application/views/ajax_so/view_so.php
@@ -1,39 +1,44 @@
 <div class="well"><h2> Sale Order Detail</h2></div>
 <div class="col-sm-12 col-md-12">
-  <?php foreach($data as $data):?>
-  <span> <a href="<?php echo base_url() ?>pdf/single_invoice_pdf/<?=$data['so_code']?>" class="w3-right"> <span class="fa fa-file-pdf-o w3-text-red fa-2x"></span> Download</a></span>
-  <span>Date: <?=$data['date']?></span>
+  <?php foreach ($data as $uperdata):?>
+    
+ 
+  <span> <a href="<?php echo base_url() ?>pdf/single_invoice_pdf/" class="w3-right"> <span class="fa fa-file-pdf-o w3-text-red fa-2x"></span> Download</a></span>
+  <span>Date: <?= $uperdata['date']?></span>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <span>SO Code:<b> <?=$data['so_code']?></b></span>
+      <span><b>SO Code:</b> <?= $uperdata['so_code']?></span>
     </div>
     <div class="form-group col-md-6">
-      <span>invoice Code: <b> <?=$data['invoice_code']?></b></span>
+      <span><b>invoice Code: </b> <?= $uperdata['invoice_code']?></span>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-4">
-      <span><b>Customer Name:</b> <?=$data['customer_name']?></span>
+      <span><b>Customer Name: </b><?= $uperdata['customer_name']?></span>
     </div>
     <div class="form-group col-md-6">
    
     </div>
+  <?php endforeach; ?>
   </div>
+</div>
   <div class="form-row">
     <table class="table table-bordered">
       <tr class="warning">
-        <td>Item Code</td>
-        <td>Item Qty</td>
-        <td>Item Rate</td>
-        <td>Profit</td>
-        <td>Total</td>
+        <th>Item Code</th>
+        <th>Item Qty</th>
+        <th>Item Rate</th>
+        <th>Profit</th>
+        <th>Total</th>
       </tr>
+      <?php foreach($data as $data):  $customer_name=$data['customer_name'];?>
       <tr>
-        <th><?=$data['item_code']?></th>
-        <th><?=$data['item_qty']?></th>
-        <th><?=$data['item_rate']?></th>
-        <th><?=$data['profit']?> %</th>
-        <th><?=$data['so_item_total']?></th>
+        <td><?=$data['item_code']?></td>
+        <td><?=$data['item_qty']?></td>
+        <td><?=$data['item_rate']?></td>
+        <td><?=$data['profit']?> %</td>
+        <td><?=$data['so_item_total']?></td>
         <?php endforeach;?>
       </tr>
     </table>
