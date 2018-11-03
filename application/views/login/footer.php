5baa4386896efc9f387d01dd0d26fe8c67549da0
MusawerAli <pakjalihouse@gmail.com>
2018-11-03 23:05:48 +0500

    write script of change body themes

--- application/views/login/footer.php
+++ application/views/login/footer.php
@@ -6,5 +6,18 @@
 
 
         <script type="text/javascript" src="<?=  base_url('asset/js/bootstrap.min.js')?>"></script>
+        <script type="text/javascript">
+        	
+        	var color=["#222f3e","#f368","#ffffff",""];
+var i=0;
+document.querySelector("#bg_change").addEventListener("click",function(){
+  i=i<color.length ? ++i :0;
+  document.querySelector("body").style.background=color[i]
+});
+
+function changeBodyBg(color){
+        document.body.style.background = color;
+    }
+        </script>
          </body>
 </html>
