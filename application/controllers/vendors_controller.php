8b625545cc353b9c16bd1267eac36164bacab2b8
MusawerAli <pakjalihouse@gmail.com>
2018-11-06 21:31:26 +0500

    Controller edit_vendor update_vendor

--- application/controllers/vendors_controller.php
+++ application/controllers/vendors_controller.php
@@ -13,5 +13,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$this->load->model('vendors_model');
 			$this->vendors_model->add_vendors();
 		}
+
+
+
+		function edit_vendor(){
+			$this->load->helper('form');
+                $vendor_id = $this->input->post('vendor_id');
+                if(isset($vendor_id) && !empty($vendor_id)){
+                $this->load->model('vendors_model');
+                $records = $this->vendors_model->edit_vendor($vendor_id);
+              
+                  $this->load->view('vendor_ajax/edit_vendor_ajax',['records'=>$records]);
+				
+		}
 	}
+
+
+
+		function update_vendor(){
+			
+			$this->load->model('vendors_model');
+			$this->vendors_model->update_vendor();
+		}
+
+		function vendor_status(){
+                    if($_POST['btn_action'] == 'delete')
+                    {
+                        $status= $this->input->post('status');
+                        if($status == 'active')
+                            {
+                            $status = 'inactive';
+                            }else{
+                                $status='active';
+                            }
+
+                            $this->load->model('vendors_model');
+                            $this->vendors_model->vendor_status($status);
+                    }
+                }
+}
  ?>
