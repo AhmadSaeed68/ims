78bd7d89443cffcf984a6f7740dae189be07e8ec
MusawerAli <pakjalihouse@gmail.com>
2018-11-06 21:34:40 +0500

    edit_vendor update_vendor _status_vendor

--- application/models/vendors_model.php
+++ application/models/vendors_model.php
@@ -34,6 +34,60 @@ class Vendors_model extends CI_Model {
 		
 	}
 
+	//*********************//
+	//Get EDIT DATA
+	//********************//
+
+		function edit_vendor($vendor_id){
+
+			
+                    $result= $this->db->select('')
+                      ->from('vendors')
+                    
+                                    ->where('id',$vendor_id)
+                                   
+                                ->get('');
+                            return $result->result_array();
+               
+		}
+
+
+
+		function update_vendor(){
+			$id=$this->input->post('id');
+			$data=array(
+				'vendor_name'=>$this->input->post('vendor_name'),
+				'manager_name'=>$this->input->post('manager_name'),
+				'city'=>$this->input->post('city'),
+				
+				'contact'=>$this->input->post('contact'),
+				'address'=>$this->input->post('address'),
+				'email'=>$this->input->post('email'),
+			);
+
+			 $q1=$this->db->where('id', $id)->update('vendors', $data);
+			 
+			 if($q1){
+			 	echo "Update Successfully";
+			 }else{
+			 	echo "Failed Due to Some Technical **Error**  @@@ Please! Contact Developer";
+			 }
+                                
+		}
+
+
+		function vendor_status($status){
+                $id= $this->input->post('id');
+                $status;
+            $update_status= $this->db
+                ->where('id', $id)
+                ->set('status', $status)
+                ->update('vendors');
+                
+                if($update_status){
+                    echo "Status Changed";
+                }
+            }
 }
 
 /* End of file vendors_model.php */
