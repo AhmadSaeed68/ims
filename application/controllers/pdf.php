1e58467a77a00ec62bd1e607fccd1b181f5a3c5b
MusawerAli <pakjalihouse@gmail.com>
2018-12-28 20:55:11 +0500

    po_order pdf

--- application/controllers/pdf.php
+++ application/controllers/pdf.php
@@ -41,6 +41,26 @@
             $this->m_pdf->pdf->Output('mpdf.pdf','D');
         }
 
+        function po_order_pdf()
+        {
+            $this->load->model('pdf_model');
+            $pdf=$this->pdf_model->po_order_pdf();
+            $html=$this->load->view('pdf/po_order_pdf',['pdf'=>$pdf],true);
+             $this->load->library('M_pdf');
+            $this->m_pdf->pdf=new mPDF('c','A4','','',32,25,27,25,16,13);
+            $this->m_pdf->pdf->defHTMLHeaderByName(
+                'myHeader2',
+                '<div style="text-align: center; font-weight: bold;">Chapter 2</div>'
+            );
+            $stylesheet = file_get_contents('../../../assets/css/bootstrap.min.css');
+            $this->m_pdf->pdf->SetDisplayMode('fullpage');
+            $this->m_pdf->pdf->list_indent_first_level = 0;
+            $this->m_pdf->pdf->WriteHTML($stylesheet,1);
+
+            $this->m_pdf->pdf->WriteHTML($html,2);
+            $this->m_pdf->pdf->Output('mpdf.pdf','D');
+        }
+
         
     }
 ?>
--- application/models/pdf_model.php
+++ application/models/pdf_model.php
@@ -11,6 +11,11 @@ return $query->result_array();
             ->query('SELECT * FROM po_invoice inner JOIN po_invoice_detail ON po_invoice_detail.invoice_code=po_invoice.invoice_code WHERE po_invoice.invoice_code AND po_invoice_detail.invoice_code='.$invoice_code.'');
 return $query->result_array();
         }
+        function po_order_pdf(){
+            $query=$this->db
+            ->query('SELECT * FROM purchase_order inner JOIN purchase_order_detail ON purchase_order_detail.po_code=purchase_order.po_code');
+return $query->result_array();
+        }
     }    
 
 ?>
