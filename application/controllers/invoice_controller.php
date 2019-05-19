02befdcedee803cf7c94cabd023908a6a5374818
MusawerAli <pakjalihouse@gmail.com>
2019-05-19 18:55:11 +0500

    add time function in constructor

--- application/controllers/invoice_controller.php
+++ application/controllers/invoice_controller.php
@@ -17,7 +17,7 @@
       $row[] = $po_invoice->invoice_code;
       $row[] = $po_invoice->invoice_total;
       $row[] = $po_invoice->invoice_description;
-      $row[] = $po_invoice->invoice_date;
+      $row[] = timeAgo($po_invoice->invoice_date);
      
       $row[]='<span class="fa fa-eye w3-text-blue"><a class="view" id="'.$po_invoice->id.'"> View</a></span>';
       $id=$this->session->userdata('user_id');
@@ -285,6 +285,34 @@ echo '<option id="'.$data["id"].'" value="'.$data["id"].'">'.$data["name"].'</op
 
          function __construct() {
         parent::__construct();
+        function timeAgo($timestamp){
+          $datetime1=new DateTime("now");
+          $datetime2=date_create($timestamp);
+          $diff=date_diff($datetime1, $datetime2);
+          $timemsg='';
+          if($diff->y > 0){
+              $timemsg = $diff->y .' year'. ($diff->y > 1?"'s":'');
+      
+          }
+          else if($diff->m > 0){
+           $timemsg = $diff->m . ' month'. ($diff->m > 1?"'s":'');
+          }
+          else if($diff->d > 0){
+           $timemsg = $diff->d .' day'. ($diff->d > 1?"'s":'');
+          }
+          else if($diff->h > 0){
+           $timemsg = $diff->h .' hour'.($diff->h > 1 ? "'s":'');
+          }
+          else if($diff->i > 0){
+           $timemsg = $diff->i .' minute'. ($diff->i > 1?"'s":'');
+          }
+          else if($diff->s > 0){
+           $timemsg = $diff->s .' second'. ($diff->s > 1?"'s":'');
+          }
+      
+      $timemsg = $timemsg.' ago';
+      return $timemsg;
+      }
         if(!$this->session->userdata('user_id')){
             redirect('login');
         }
